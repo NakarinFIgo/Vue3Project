874a9c0d41e4eb973e82c8ccef4129ac20cb87f4
FIGO <140080105+NakarinFIgo@users.noreply.github.com>
2024-11-02 23:54:19 +0700

    feat : initial project

---
+++ vue-bun-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit architecto voluptatem atque aliquid nihil perferendis alias cupiditate corporis voluptatum. Incidunt quam deleniti ipsam sed magnam!</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
